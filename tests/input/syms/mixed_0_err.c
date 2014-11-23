689572bedd97c26130ace0f0d8b11844091efad0
troybowman <troybowman2000@gmail.com>
2014-11-23 20:32:07 +0100

    + test for invalid function definitions/declarations

---
+++ tests/input/syms/mixed_0_err.c
@@ -0,0 +1,69 @@
+void func1(int x, char y)
+{
+  char array[];
+  char array[45];
+}
+
+char array[35];
+
+void func2(void) {}
+void func3(void) {}
+void func4(void) {}
+void func5(void) {}
+void func6(void) {}
+
+char func7(void)
+{
+  char func8(void);
+  char func8;
+  char func9[];
+  char func9;
+}
+
+/* recoverable redeclaration errors */
+int func2(char x), func3(char x), func4(char x), func5(char x), func6(int buf[]);
+
+int func7;
+
+()
+  just
+some junk
+
+
+void missing_open_bracket(int x)
+
+  asdflkhjasd
+  asdflkjhasd
+}
+
+int func8()
+{
+  intad
+    kjasdhkljh
+    klajsd
+    hasdfalk
+  43453
+}
+
+/*
+func8(void)
+{
+  this too
+}
+
+void func8(void)
+{
+  int x;
+}
+
+int array(void);
+
+int array(void)
+{
+
+}
+*/
+
+this doesn't matter
+we terminate after a function
+definition is invalidated
