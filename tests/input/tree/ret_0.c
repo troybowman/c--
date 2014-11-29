590deee8ad32f5c8c5c830e31aa7a63faac1d102
troybowman <troybowman2000@gmail.com>
2014-11-29 19:37:00 +0100

    + test for simple return statements

---
+++ tests/input/tree/ret_0.c
@@ -0,0 +1,7 @@
+/* simple return values */
+
+int get5(void) { return 5; }
+
+char getc(void) { return 'c'; }
+
+void get_nothing(void) { return; }
---
+++ tests/output/tree/ret_0.s
@@ -0,0 +1,60 @@
+#-----------------------------------------------------------------------------
+# GLOBAL SYMBOL TABLE
+#-----------------------------------------------------------------------------
+# size: 3
+# sym: get5
+#   line: 3
+#   type: ST_FUNCTION
+#     rt_type: RT_INT
+#     params:
+#       none
+#     is_extern: no
+# sym: get_nothing
+#   line: 7
+#   type: ST_FUNCTION
+#     rt_type: RT_VOID
+#     params:
+#       none
+#     is_extern: no
+# sym: getc
+#   line: 5
+#   type: ST_FUNCTION
+#     rt_type: RT_CHAR
+#     params:
+#       none
+#     is_extern: no
+#-----------------------------------------------------------------------------
+# LOCAL SYMBOLS FOR FUNCTION: get5
+#-----------------------------------------------------------------------------
+# size: 0
+#-----------------------------------------------------------------------------
+# SYNTAX TREE FOR FUNCTION: get5
+#-----------------------------------------------------------------------------
+# node 1: type: TNT_STMT
+# child SEQ_CUR for node 1
+# node 2: type: TNT_RET
+# child RET_EXPR for node 2
+# node 3: type: TNT_INTCON val: 5
+#-----------------------------------------------------------------------------
+# LOCAL SYMBOLS FOR FUNCTION: getc
+#-----------------------------------------------------------------------------
+# size: 0
+#-----------------------------------------------------------------------------
+# SYNTAX TREE FOR FUNCTION: getc
+#-----------------------------------------------------------------------------
+# node 1: type: TNT_STMT
+# child SEQ_CUR for node 1
+# node 2: type: TNT_RET
+# child RET_EXPR for node 2
+# node 3: type: TNT_CHARCON str: 'c'
+#-----------------------------------------------------------------------------
+# LOCAL SYMBOLS FOR FUNCTION: get_nothing
+#-----------------------------------------------------------------------------
+# size: 0
+#-----------------------------------------------------------------------------
+# SYNTAX TREE FOR FUNCTION: get_nothing
+#-----------------------------------------------------------------------------
+# node 1: type: TNT_STMT
+# child SEQ_CUR for node 1
+# node 2: type: TNT_RET
+# c-- exited with code: 0
