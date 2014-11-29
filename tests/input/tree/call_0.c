5fc463d06393bd26d13d072851053fba99bcf0c6
troybowman <troybowman2000@gmail.com>
2014-11-29 12:13:34 +0100

    + beefed up test call_0.c to test assorted constant function arguments

--- tests/input/tree/call_0.c
+++ tests/input/tree/call_0.c
@@ -1,6 +1,21 @@
-void print_string(char str[]) {}
+/* simple constant arguments */
+
+extern void print_string(char str[]);
+extern void print_strings(
+    char str1[],
+    char str2[],
+    char str3[],
+    char str4[]);
+extern void print_int(int x);
+extern void print_ints(int x, int y, int z);
+extern void print_misc(char one[], char two, char three[], int four);
 
 void main(void)
 {
   print_string("hello");
+  print_int(5);
+  print_int('x');
+  print_strings("one", "two", "three", "four");
+  print_ints(1, 2, '3');
+  print_misc("one", '2', "three", 4);
 }
--- tests/output/tree/call_0.s
+++ tests/output/tree/call_0.s
@@ -1,35 +1,83 @@
 #-----------------------------------------------------------------------------
 # GLOBAL SYMBOL TABLE
 #-----------------------------------------------------------------------------
-# size: 2
+# size: 6
 # sym: main
-#   line: 3
+#   line: 13
 #   type: ST_FUNCTION
 #     rt_type: RT_VOID
 #     params:
 #       none
 #     is_extern: no
+# sym: print_int
+#   line: 9
+#   type: ST_FUNCTION
+#     rt_type: RT_VOID
+#     params:
+#       0: x
+#         type: ST_PRIMITIVE
+#           base: PRIM_INT
+#     is_extern: yes
+# sym: print_ints
+#   line: 10
+#   type: ST_FUNCTION
+#     rt_type: RT_VOID
+#     params:
+#       0: x
+#         type: ST_PRIMITIVE
+#           base: PRIM_INT
+#       1: y
+#         type: ST_PRIMITIVE
+#           base: PRIM_INT
+#       2: z
+#         type: ST_PRIMITIVE
+#           base: PRIM_INT
+#     is_extern: yes
+# sym: print_misc
+#   line: 11
+#   type: ST_FUNCTION
+#     rt_type: RT_VOID
+#     params:
+#       0: one
+#         type: ST_ARRAY
+#           base: PRIM_CHAR
+#       1: two
+#         type: ST_PRIMITIVE
+#           base: PRIM_CHAR
+#       2: three
+#         type: ST_ARRAY
+#           base: PRIM_CHAR
+#       3: four
+#         type: ST_PRIMITIVE
+#           base: PRIM_INT
+#     is_extern: yes
 # sym: print_string
-#   line: 1
+#   line: 3
 #   type: ST_FUNCTION
 #     rt_type: RT_VOID
 #     params:
 #       0: str
 #         type: ST_ARRAY
 #           base: PRIM_CHAR
-#     is_extern: no
-#-----------------------------------------------------------------------------
-# LOCAL SYMBOLS FOR FUNCTION: print_string
-#-----------------------------------------------------------------------------
-# size: 1
-# sym: str
-#   line: 1
-#   type: ST_ARRAY
-#     base: PRIM_CHAR
-#     size: 0xffffffff
-#-----------------------------------------------------------------------------
-# SYNTAX TREE FOR FUNCTION: print_string
-#-----------------------------------------------------------------------------
+#     is_extern: yes
+# sym: print_strings
+#   line: 8
+#   type: ST_FUNCTION
+#     rt_type: RT_VOID
+#     params:
+#       0: str1
+#         type: ST_ARRAY
+#           base: PRIM_CHAR
+#       1: str2
+#         type: ST_ARRAY
+#           base: PRIM_CHAR
+#       2: str3
+#         type: ST_ARRAY
+#           base: PRIM_CHAR
+#       3: str4
+#         type: ST_ARRAY
+#           base: PRIM_CHAR
+#     is_extern: yes
 #-----------------------------------------------------------------------------
 # LOCAL SYMBOLS FOR FUNCTION: main
 #-----------------------------------------------------------------------------
@@ -44,4 +92,76 @@
 # node 3: type: TNT_ARG
 # child 0 for node 3:
 # node 4: type: TNT_STRCON str: "hello"
+# child 1 for node 1:
+# node 5: type: TNT_STMT
+# child 0 for node 5:
+# node 6: type: TNT_CALL sym: print_int
+# child 0 for node 6:
+# node 7: type: TNT_ARG
+# child 0 for node 7:
+# node 8: type: TNT_INTCON val: 5
+# child 1 for node 5:
+# node 9: type: TNT_STMT
+# child 0 for node 9:
+# node 10: type: TNT_CALL sym: print_int
+# child 0 for node 10:
+# node 11: type: TNT_ARG
+# child 0 for node 11:
+# node 12: type: TNT_CHARCON str: 'x'
+# child 1 for node 9:
+# node 13: type: TNT_STMT
+# child 0 for node 13:
+# node 14: type: TNT_CALL sym: print_strings
+# child 0 for node 14:
+# node 15: type: TNT_ARG
+# child 0 for node 15:
+# node 16: type: TNT_STRCON str: "one"
+# child 1 for node 15:
+# node 17: type: TNT_ARG
+# child 0 for node 17:
+# node 18: type: TNT_STRCON str: "two"
+# child 1 for node 17:
+# node 19: type: TNT_ARG
+# child 0 for node 19:
+# node 20: type: TNT_STRCON str: "three"
+# child 1 for node 19:
+# node 21: type: TNT_ARG
+# child 0 for node 21:
+# node 22: type: TNT_STRCON str: "four"
+# child 1 for node 13:
+# node 23: type: TNT_STMT
+# child 0 for node 23:
+# node 24: type: TNT_CALL sym: print_ints
+# child 0 for node 24:
+# node 25: type: TNT_ARG
+# child 0 for node 25:
+# node 26: type: TNT_INTCON val: 1
+# child 1 for node 25:
+# node 27: type: TNT_ARG
+# child 0 for node 27:
+# node 28: type: TNT_INTCON val: 2
+# child 1 for node 27:
+# node 29: type: TNT_ARG
+# child 0 for node 29:
+# node 30: type: TNT_CHARCON str: '3'
+# child 1 for node 23:
+# node 31: type: TNT_STMT
+# child 0 for node 31:
+# node 32: type: TNT_CALL sym: print_misc
+# child 0 for node 32:
+# node 33: type: TNT_ARG
+# child 0 for node 33:
+# node 34: type: TNT_STRCON str: "one"
+# child 1 for node 33:
+# node 35: type: TNT_ARG
+# child 0 for node 35:
+# node 36: type: TNT_CHARCON str: '2'
+# child 1 for node 35:
+# node 37: type: TNT_ARG
+# child 0 for node 37:
+# node 38: type: TNT_STRCON str: "three"
+# child 1 for node 37:
+# node 39: type: TNT_ARG
+# child 0 for node 39:
+# node 40: type: TNT_INTCON val: 4
 # c-- exited with code: 0
