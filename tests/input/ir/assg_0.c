f0fa88c1e49bc47bc7bf0d87aa8629e1a5d1c08b
troybowman <troybowman2000@gmail.com>
2014-12-13 22:57:53 +0100

    append to ir test for simple assignments

--- tests/input/ir/assg_0.c
+++ tests/input/ir/assg_0.c
@@ -1,7 +1,14 @@
-/* simple assignment */
+/* simple assignments */
+
+int g1;
+char g2[10];
 
 void main(void)
 {
-  int x;
-  x = 5;
+  int l1, l2[10];
+
+  g1     = 5;
+  g2[0]  = 'a';
+  l1     = 5555;
+  l2[10] = g2[3];
 }
--- tests/output/ir/assg_0.s
+++ tests/output/ir/assg_0.s
@@ -1,9 +1,18 @@
 #-----------------------------------------------------------------------------
 # GLOBAL SYMBOL TABLE
 #-----------------------------------------------------------------------------
-# size: 1
-# sym: main
+# size: 3
+# sym: g1
 #   line: 3
+#   type: ST_PRIMITIVE
+#     base: PRIM_INT
+# sym: g2
+#   line: 4
+#   type: ST_ARRAY
+#     base: PRIM_CHAR
+#     size: 0xa
+# sym: main
+#   line: 6
 #   type: ST_FUNCTION
 #     rt_type: RT_VOID
 #     params:
@@ -12,11 +21,16 @@
 #-----------------------------------------------------------------------------
 # LOCAL SYMBOLS FOR FUNCTION: main
 #-----------------------------------------------------------------------------
-# size: 1
-# sym: x
-#   line: 5
+# size: 2
+# sym: l1
+#   line: 8
 #   type: ST_PRIMITIVE
 #     base: PRIM_INT
+# sym: l2
+#   line: 8
+#   type: ST_ARRAY
+#     base: PRIM_INT
+#     size: 0xa
 #-----------------------------------------------------------------------------
 # SYNTAX TREE FOR FUNCTION: main
 #-----------------------------------------------------------------------------
@@ -24,9 +38,39 @@
 # child SEQ_CUR for node 1
 # node 2: type: TNT_ASSG
 # child LHS for node 2
-# node 3: type: TNT_SYMBOL sym: x
+# node 3: type: TNT_SYMBOL sym: g1
 # child RHS for node 2
 # node 4: type: TNT_INTCON val: 5
+# child SEQ_NEXT for node 1
+# node 5: type: TNT_STMT
+# child SEQ_CUR for node 5
+# node 6: type: TNT_ASSG
+# child LHS for node 6
+# node 7: type: TNT_ARRAY_LOOKUP sym: g2
+# child AL_OFFSET for node 7
+# node 8: type: TNT_INTCON val: 0
+# child RHS for node 6
+# node 9: type: TNT_CHARCON str: 'a'
+# child SEQ_NEXT for node 5
+# node 10: type: TNT_STMT
+# child SEQ_CUR for node 10
+# node 11: type: TNT_ASSG
+# child LHS for node 11
+# node 12: type: TNT_SYMBOL sym: l1
+# child RHS for node 11
+# node 13: type: TNT_INTCON val: 5555
+# child SEQ_NEXT for node 10
+# node 14: type: TNT_STMT
+# child SEQ_CUR for node 14
+# node 15: type: TNT_ASSG
+# child LHS for node 15
+# node 16: type: TNT_ARRAY_LOOKUP sym: l2
+# child AL_OFFSET for node 16
+# node 17: type: TNT_INTCON val: 10
+# child RHS for node 15
+# node 18: type: TNT_ARRAY_LOOKUP sym: g2
+# child AL_OFFSET for node 18
+# node 19: type: TNT_INTCON val: 3
 #-----------------------------------------------------------------------------
 # INTERMEDIATE CODE FOR FUNCTION: main
 #-----------------------------------------------------------------------------
@@ -38,6 +82,99 @@
 # >
 # CNT_SW
 # ------
-# dest -> ST_PRIMITIVE (x)
+# dest -> ST_PRIMITIVE (g1)
 # src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_MOV
+# ------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_IMMEDIATE_CHAR ('a')
+# |
+# >
+# CNT_LEA
+# ------
+# dest -> ST_TEMPORARY (2)
+# src1 -> ST_ARRAY (g2)
+# |
+# >
+# CNT_MOV
+# ------
+# dest -> ST_TEMPORARY (3)
+# src1 -> ST_IMMEDIATE_INT (0)
+# |
+# >
+# CNT_ADD
+# ------
+# dest -> ST_TEMPORARY (4)
+# src1 -> ST_TEMPORARY (2)
+# src2 -> ST_TEMPORARY (3)
+# |
+# >
+# CNT_SB
+# ------
+# dest -> ST_TEMPORARY (4)
+# src1 -> ST_TEMPORARY (1)
+# |
+# >
+# CNT_MOV
+# ------
+# dest -> ST_TEMPORARY (5)
+# src1 -> ST_IMMEDIATE_INT (5555)
+# |
+# >
+# CNT_SW
+# ------
+# dest -> ST_PRIMITIVE (l1)
+# src1 -> ST_TEMPORARY (5)
+# |
+# >
+# CNT_LEA
+# ------
+# dest -> ST_TEMPORARY (6)
+# src1 -> ST_ARRAY (g2)
+# |
+# >
+# CNT_MOV
+# ------
+# dest -> ST_TEMPORARY (7)
+# src1 -> ST_IMMEDIATE_INT (3)
+# |
+# >
+# CNT_ADD
+# ------
+# dest -> ST_TEMPORARY (8)
+# src1 -> ST_TEMPORARY (6)
+# src2 -> ST_TEMPORARY (7)
+# |
+# >
+# CNT_LB
+# ------
+# dest -> ST_TEMPORARY (9)
+# src1 -> ST_TEMPORARY (8)
+# |
+# >
+# CNT_LEA
+# ------
+# dest -> ST_TEMPORARY (10)
+# src1 -> ST_ARRAY (l2)
+# |
+# >
+# CNT_MOV
+# ------
+# dest -> ST_TEMPORARY (11)
+# src1 -> ST_IMMEDIATE_INT (10)
+# |
+# >
+# CNT_ADD
+# ------
+# dest -> ST_TEMPORARY (12)
+# src1 -> ST_TEMPORARY (10)
+# src2 -> ST_TEMPORARY (11)
+# |
+# >
+# CNT_SW
+# ------
+# dest -> ST_TEMPORARY (12)
+# src1 -> ST_TEMPORARY (9)
 # c-- exited with code: 0
