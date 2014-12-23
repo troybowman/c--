117d1cf20c3f4c67ce897b5a5912f96caa514581
troybowman <troybowman2000@gmail.com>
2014-12-23 13:34:58 +0100

    test for an empty else clause

---
+++ tests/input/tree/if_6.c
@@ -0,0 +1,11 @@
+/* empty else */
+
+void main(void)
+{
+  if ( 5 == 5 )
+    return;
+
+  if ( 6 == 6 )
+    return;
+  else {}
+}
---
+++ tests/output/tree/if_6.s
@@ -0,0 +1,42 @@
+#-----------------------------------------------------------------------------
+# GLOBAL SYMBOL TABLE
+#-----------------------------------------------------------------------------
+# size: 1
+# sym: main
+#   line: 3
+#   type: ST_FUNCTION
+#     rt_type: RT_VOID
+#     params:
+#       none
+#     is_extern: no
+#-----------------------------------------------------------------------------
+# LOCAL SYMBOLS FOR FUNCTION: main
+#-----------------------------------------------------------------------------
+# size: 0
+#-----------------------------------------------------------------------------
+# SYNTAX TREE FOR FUNCTION: main
+#-----------------------------------------------------------------------------
+# node 1: type: TNT_STMT
+# child SEQ_CUR for node 1
+# node 2: type: TNT_IF
+# child IF_COND for node 2
+# node 3: type: TNT_EQ
+# child LHS for node 3
+# node 4: type: TNT_INTCON val: 5
+# child RHS for node 3
+# node 5: type: TNT_INTCON val: 5
+# child IF_BODY for node 2
+# node 6: type: TNT_RET
+# child SEQ_NEXT for node 1
+# node 7: type: TNT_STMT
+# child SEQ_CUR for node 7
+# node 8: type: TNT_IF
+# child IF_COND for node 8
+# node 9: type: TNT_EQ
+# child LHS for node 9
+# node 10: type: TNT_INTCON val: 6
+# child RHS for node 9
+# node 11: type: TNT_INTCON val: 6
+# child IF_BODY for node 8
+# node 12: type: TNT_RET
+# c-- exited with code: 0
