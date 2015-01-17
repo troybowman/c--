57d4dc1238520425846a47eb8beb02cca1bf917b
troy <troybowman2000@gmail.com>
2015-01-17 20:30:01 +0100

    added test for zero length array

--- tests/input/mips/frame_3.c
+++ tests/input/mips/frame_3.c
@@ -13,7 +13,7 @@ void simple(int one, int two, int three, int four) {}
 void another(char one, char two[])
 {
   char array[3];
-  int array1[1], array2[3];
+  int array1[1], array2[0], array3[3];
   simple(1, 2, 3, 4);
 }
 
