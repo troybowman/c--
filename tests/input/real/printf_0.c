4c24996bfe337b39465312ddd61450ce3c8e07b7
troybowman <troybowman2000@gmail.com>
2015-02-21 03:03:08 +0100

    move real tests to temp directory for now

---
+++ tests/input/real/printf_0.c
@@ -0,0 +1,17 @@
+extern void printf(char format[], ...);
+
+void main(void)
+{
+  int x;
+  char c;
+
+  x = 1;
+  c = 'a';
+
+  printf("one: %d\n", x)
+  printf("two: %c\n", c)
+  printf("three %s\n", "hello");
+
+  printf("all together: (%d, %c, %s)\n",
+          x, c, "hello again");
+}
