a38afcfdc9552f3f7c8f84c9af5a189b5e5c8ba7
troybowman <troybowman2000@gmail.com>
2015-02-21 23:41:06 +0100

    update test, todo

--- tests/input/real/printf_0.c
+++ tests/input/real/printf_0.c
@@ -5,13 +5,14 @@ void main(void)
   int x;
   char c;
 
-  x = 1;
+  x = 66;
   c = 'a';
 
   printf("one: %d\n", x);
   printf("two: %c\n", c);
   printf("three: %s\n", "hello");
-
-  printf("all together: (%d, %c, %s)\n",
-          x, c, "hello again");
+  printf("all together: (%d, %c, %s)\n", x, c, "hello again");
+  printf("");
+  printf("%s", "");
+  printf("c%cc%%%d%d%%%cccc%ds%ss%c%dd\n", x, x, x, x, c, "", c, c);
 }
--- tests/output/real/printf_0.asm
+++ tests/output/real/printf_0.asm
@@ -67,7 +67,7 @@
 # child LHS for node 2
 # node 3: type: TNT_SYMBOL sym: x
 # child RHS for node 2
-# node 4: type: TNT_INTCON val: 1
+# node 4: type: TNT_INTCON val: 66
 # child SEQ_NEXT for node 1
 # node 5: type: TNT_STMT
 # child SEQ_CUR for node 5
@@ -220,6 +220,154 @@
 # node 79: type: TNT_ARG
 # child SEQ_CUR for node 79
 # node 80: type: TNT_STRCON str: ")\n"
+# child SEQ_NEXT for node 51
+# node 81: type: TNT_STMT
+# child SEQ_CUR for node 81
+# node 82: type: TNT_PRINTF
+# child PRINTF_TREE for node 82
+# node 83: type: TNT_STMT
+# child SEQ_CUR for node 83
+# node 84: type: TNT_CALL sym: _print_string
+# child CALL_ARGS for node 84
+# node 85: type: TNT_ARG
+# child SEQ_CUR for node 85
+# node 86: type: TNT_STRCON str: ""
+# child SEQ_NEXT for node 81
+# node 87: type: TNT_STMT
+# child SEQ_CUR for node 87
+# node 88: type: TNT_PRINTF
+# child PRINTF_TREE for node 88
+# node 89: type: TNT_STMT
+# child SEQ_CUR for node 89
+# node 90: type: TNT_CALL sym: _print_string
+# child CALL_ARGS for node 90
+# node 91: type: TNT_ARG
+# child SEQ_CUR for node 91
+# node 92: type: TNT_STRCON str: ""
+# child SEQ_NEXT for node 87
+# node 93: type: TNT_STMT
+# child SEQ_CUR for node 93
+# node 94: type: TNT_PRINTF
+# child PRINTF_TREE for node 94
+# node 95: type: TNT_STMT
+# child SEQ_CUR for node 95
+# node 96: type: TNT_CALL sym: _print_string
+# child CALL_ARGS for node 96
+# node 97: type: TNT_ARG
+# child SEQ_CUR for node 97
+# node 98: type: TNT_STRCON str: "c"
+# child SEQ_NEXT for node 95
+# node 99: type: TNT_STMT
+# child SEQ_CUR for node 99
+# node 100: type: TNT_CALL sym: _print_char
+# child CALL_ARGS for node 100
+# node 101: type: TNT_ARG
+# child SEQ_CUR for node 101
+# node 102: type: TNT_SYMBOL sym: x
+# child SEQ_NEXT for node 99
+# node 103: type: TNT_STMT
+# child SEQ_CUR for node 103
+# node 104: type: TNT_CALL sym: _print_string
+# child CALL_ARGS for node 104
+# node 105: type: TNT_ARG
+# child SEQ_CUR for node 105
+# node 106: type: TNT_STRCON str: "c%%"
+# child SEQ_NEXT for node 103
+# node 107: type: TNT_STMT
+# child SEQ_CUR for node 107
+# node 108: type: TNT_CALL sym: _print_int
+# child CALL_ARGS for node 108
+# node 109: type: TNT_ARG
+# child SEQ_CUR for node 109
+# node 110: type: TNT_SYMBOL sym: x
+# child SEQ_NEXT for node 107
+# node 111: type: TNT_STMT
+# child SEQ_CUR for node 111
+# node 112: type: TNT_CALL sym: _print_int
+# child CALL_ARGS for node 112
+# node 113: type: TNT_ARG
+# child SEQ_CUR for node 113
+# node 114: type: TNT_SYMBOL sym: x
+# child SEQ_NEXT for node 111
+# node 115: type: TNT_STMT
+# child SEQ_CUR for node 115
+# node 116: type: TNT_CALL sym: _print_string
+# child CALL_ARGS for node 116
+# node 117: type: TNT_ARG
+# child SEQ_CUR for node 117
+# node 118: type: TNT_STRCON str: "%%"
+# child SEQ_NEXT for node 115
+# node 119: type: TNT_STMT
+# child SEQ_CUR for node 119
+# node 120: type: TNT_CALL sym: _print_char
+# child CALL_ARGS for node 120
+# node 121: type: TNT_ARG
+# child SEQ_CUR for node 121
+# node 122: type: TNT_SYMBOL sym: x
+# child SEQ_NEXT for node 119
+# node 123: type: TNT_STMT
+# child SEQ_CUR for node 123
+# node 124: type: TNT_CALL sym: _print_string
+# child CALL_ARGS for node 124
+# node 125: type: TNT_ARG
+# child SEQ_CUR for node 125
+# node 126: type: TNT_STRCON str: "ccc"
+# child SEQ_NEXT for node 123
+# node 127: type: TNT_STMT
+# child SEQ_CUR for node 127
+# node 128: type: TNT_CALL sym: _print_int
+# child CALL_ARGS for node 128
+# node 129: type: TNT_ARG
+# child SEQ_CUR for node 129
+# node 130: type: TNT_SYMBOL sym: c
+# child SEQ_NEXT for node 127
+# node 131: type: TNT_STMT
+# child SEQ_CUR for node 131
+# node 132: type: TNT_CALL sym: _print_string
+# child CALL_ARGS for node 132
+# node 133: type: TNT_ARG
+# child SEQ_CUR for node 133
+# node 134: type: TNT_STRCON str: "s"
+# child SEQ_NEXT for node 131
+# node 135: type: TNT_STMT
+# child SEQ_CUR for node 135
+# node 136: type: TNT_CALL sym: _print_string
+# child CALL_ARGS for node 136
+# node 137: type: TNT_ARG
+# child SEQ_CUR for node 137
+# node 138: type: TNT_STRCON str: ""
+# child SEQ_NEXT for node 135
+# node 139: type: TNT_STMT
+# child SEQ_CUR for node 139
+# node 140: type: TNT_CALL sym: _print_string
+# child CALL_ARGS for node 140
+# node 141: type: TNT_ARG
+# child SEQ_CUR for node 141
+# node 142: type: TNT_STRCON str: "s"
+# child SEQ_NEXT for node 139
+# node 143: type: TNT_STMT
+# child SEQ_CUR for node 143
+# node 144: type: TNT_CALL sym: _print_char
+# child CALL_ARGS for node 144
+# node 145: type: TNT_ARG
+# child SEQ_CUR for node 145
+# node 146: type: TNT_SYMBOL sym: c
+# child SEQ_NEXT for node 143
+# node 147: type: TNT_STMT
+# child SEQ_CUR for node 147
+# node 148: type: TNT_CALL sym: _print_int
+# child CALL_ARGS for node 148
+# node 149: type: TNT_ARG
+# child SEQ_CUR for node 149
+# node 150: type: TNT_SYMBOL sym: c
+# child SEQ_NEXT for node 147
+# node 151: type: TNT_STMT
+# child SEQ_CUR for node 151
+# node 152: type: TNT_CALL sym: _print_string
+# child CALL_ARGS for node 152
+# node 153: type: TNT_ARG
+# child SEQ_CUR for node 153
+# node 154: type: TNT_STRCON str: "d\n"
 #-----------------------------------------------------------------------------
 # INTERMEDIATE CODE FOR FUNCTION: main
 #-----------------------------------------------------------------------------
@@ -231,7 +379,7 @@
 # CNT_LI
 # ------
 # dest -> ST_TEMPORARY (0)
-# src1 -> ST_INTCON (1)
+# src1 -> ST_INTCON (66)
 # |
 # >
 # CNT_SW
@@ -522,6 +670,295 @@
 # CNT_CALL
 # --------
 # src1 -> ST_FUNCTION (_print_string)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_STRCON ("")
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_string)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_STRCON ("")
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_string)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_STRCON ("c")
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_string)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (x)
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_char)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_STRCON ("c%%")
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_string)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (x)
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_int)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (x)
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_int)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_STRCON ("%%")
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_string)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (x)
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_char)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_STRCON ("ccc")
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_string)
+# |
+# >
+# CNT_LB
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (c)
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_int)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_STRCON ("s")
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_string)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_STRCON ("")
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_string)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_STRCON ("s")
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_string)
+# |
+# >
+# CNT_LB
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (c)
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_char)
+# |
+# >
+# CNT_LB
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (c)
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_int)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_STRCON ("d\n")
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_string)
 
 .data
 
@@ -561,6 +998,34 @@
     .asciiz ")\n"
     .align 2
 
+  _str9:
+    .asciiz ""
+    .align 2
+
+  _str10:
+    .asciiz "c"
+    .align 2
+
+  _str11:
+    .asciiz "c%%"
+    .align 2
+
+  _str12:
+    .asciiz "%%"
+    .align 2
+
+  _str13:
+    .asciiz "ccc"
+    .align 2
+
+  _str14:
+    .asciiz "s"
+    .align 2
+
+  _str15:
+    .asciiz "d\n"
+    .align 2
+
 .text
 
 main:
@@ -579,7 +1044,7 @@ main:
   la $sp, -32($sp)
   sw $ra, 16($sp)
 
-  li $t0, 1
+  li $t0, 66
   sw $t0, 24($sp)
   li, $t0, 'a'
   sb $t0, 28($sp)
@@ -629,6 +1094,57 @@ main:
   move $a0, $t0
   jal __print_string
   la $t0, _str8
+  move $a0, $t0
+  jal __print_string
+  la $t0, _str9
+  move $a0, $t0
+  jal __print_string
+  la $t0, _str9
+  move $a0, $t0
+  jal __print_string
+  la $t0, _str10
+  move $a0, $t0
+  jal __print_string
+  lw $t0, 24($sp)
+  move $a0, $t0
+  jal __print_char
+  la $t0, _str11
+  move $a0, $t0
+  jal __print_string
+  lw $t0, 24($sp)
+  move $a0, $t0
+  jal __print_int
+  lw $t0, 24($sp)
+  move $a0, $t0
+  jal __print_int
+  la $t0, _str12
+  move $a0, $t0
+  jal __print_string
+  lw $t0, 24($sp)
+  move $a0, $t0
+  jal __print_char
+  la $t0, _str13
+  move $a0, $t0
+  jal __print_string
+  lb $t0, 28($sp)
+  move $a0, $t0
+  jal __print_int
+  la $t0, _str14
+  move $a0, $t0
+  jal __print_string
+  la $t0, _str9
+  move $a0, $t0
+  jal __print_string
+  la $t0, _str14
+  move $a0, $t0
+  jal __print_string
+  lb $t0, 28($sp)
+  move $a0, $t0
+  jal __print_char
+  lb $t0, 28($sp)
+  move $a0, $t0
+  jal __print_int
+  la $t0, _str15
   move $a0, $t0
   jal __print_string
 
