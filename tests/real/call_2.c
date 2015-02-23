452dc03e46ccd124208c93a5a3e570079c53772a
troybowman <troybowman2000@gmail.com>
2015-02-24 00:56:59 +0100

    + test for nested function calls

--- tests/real/call_2.c
+++ tests/real/call_2.c
@@ -5,8 +5,7 @@ int add(int x, int y)
   return x+y;
 }
 
-extern void print_int (int n);
-extern void print_string (char s[]);
+extern void printf(char format[], ...);
 
 void main(void)
 {
@@ -17,6 +16,6 @@ void main(void)
   for (i = 7; i > 0; i = i-1) {
     f = f*add(add(1, add(add(2, add(add(i, 0), 1)), -5)), 1);
   }
-  print_int(f);
-  print_string("\n");  
+
+  printf("%d\n", f);
 }
