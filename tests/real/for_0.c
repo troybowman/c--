40a5ebe4538c1090fc3f7cd999bed2b892a7cf05
troybowman <troybowman2000@gmail.com>
2015-02-24 00:05:15 +0100

    + test for simple for loops

--- tests/real/for_0.c
+++ tests/real/for_0.c
@@ -1,7 +1,6 @@
 /* nested for loops */
 
-extern void print_int (int n);
-extern void print_string (char s[]);
+extern void printf(char format[], ...);
 
 void main(void)
 {
@@ -12,6 +11,6 @@ void main(void)
     for (j = 0; j < 10; j = j+2)
       for (k = 0; k < 15; k = k+3)
 	res = res + (i+j+k)/3;
-  print_int(res);
-  print_string("\n");
+
+  printf("%d\n", res);
 }
