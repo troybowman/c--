9a5432669f0a2d1e821982b6565b2d2e6c3b0f83
troybowman <troybowman2000@gmail.com>
2015-02-23 23:44:11 +0100

    + correct asm file for real/while_0.c

---
+++ tests/real/while_0.asm
@@ -0,0 +1,307 @@
+#-----------------------------------------------------------------------------
+# GLOBAL SYMBOL TABLE
+#-----------------------------------------------------------------------------
+# size: 6
+# sym: i
+#   line: 3
+#   type: ST_PRIMITIVE
+#     base: PRIM_INT
+# sym: _print_int
+#   line: -1
+#   type: ST_FUNCTION
+#     rt_type: RT_VOID
+#     params:
+#       0: val
+#         type: ST_PRIMITIVE
+#           base: PRIM_INT
+#     is_extern: yes
+# sym: _print_string
+#   line: -1
+#   type: ST_FUNCTION
+#     rt_type: RT_VOID
+#     params:
+#       0: str
+#         type: ST_ARRAY
+#           base: PRIM_CHAR
+#     is_extern: yes
+# sym: _print_char
+#   line: -1
+#   type: ST_FUNCTION
+#     rt_type: RT_VOID
+#     params:
+#       0: c
+#         type: ST_PRIMITIVE
+#           base: PRIM_CHAR
+#     is_extern: yes
+# sym: printf
+#   line: 5
+#   type: ST_FUNCTION
+#     rt_type: RT_VOID
+#     params:
+#       0: format
+#         type: ST_ARRAY
+#           base: PRIM_CHAR
+#       1: ST_ELLIPSIS
+#     is_extern: yes
+# sym: main
+#   line: 7
+#   type: ST_FUNCTION
+#     rt_type: RT_VOID
+#     params:
+#       none
+#     is_extern: no
+#-----------------------------------------------------------------------------
+# LOCAL SYMBOLS FOR FUNCTION: main
+#-----------------------------------------------------------------------------
+# size: 0
+#-----------------------------------------------------------------------------
+# SYNTAX TREE FOR FUNCTION: main
+#-----------------------------------------------------------------------------
+# node 1: type: TNT_STMT
+# child SEQ_CUR for node 1
+# node 2: type: TNT_ASSG
+# child LHS for node 2
+# node 3: type: TNT_SYMBOL sym: i
+# child RHS for node 2
+# node 4: type: TNT_INTCON val: 0
+# child SEQ_NEXT for node 1
+# node 5: type: TNT_STMT
+# child SEQ_CUR for node 5
+# node 6: type: TNT_WHILE
+# child WHILE_COND for node 6
+# node 7: type: TNT_LT
+# child LHS for node 7
+# node 8: type: TNT_SYMBOL sym: i
+# child RHS for node 7
+# node 9: type: TNT_INTCON val: 100
+# child WHILE_BODY for node 6
+# node 10: type: TNT_STMT
+# child SEQ_CUR for node 10
+# node 11: type: TNT_PRINTF
+# child PRINTF_TREE for node 11
+# node 12: type: TNT_STMT
+# child SEQ_CUR for node 12
+# node 13: type: TNT_CALL sym: _print_int
+# child CALL_ARGS for node 13
+# node 14: type: TNT_ARG
+# child SEQ_CUR for node 14
+# node 15: type: TNT_SYMBOL sym: i
+# child SEQ_NEXT for node 12
+# node 16: type: TNT_STMT
+# child SEQ_CUR for node 16
+# node 17: type: TNT_CALL sym: _print_string
+# child CALL_ARGS for node 17
+# node 18: type: TNT_ARG
+# child SEQ_CUR for node 18
+# node 19: type: TNT_STRCON str: "\n"
+# child SEQ_NEXT for node 10
+# node 20: type: TNT_STMT
+# child SEQ_CUR for node 20
+# node 21: type: TNT_ASSG
+# child LHS for node 21
+# node 22: type: TNT_SYMBOL sym: i
+# child RHS for node 21
+# node 23: type: TNT_PLUS
+# child LHS for node 23
+# node 24: type: TNT_PLUS
+# child LHS for node 24
+# node 25: type: TNT_SYMBOL sym: i
+# child RHS for node 24
+# node 26: type: TNT_SYMBOL sym: i
+# child RHS for node 23
+# node 27: type: TNT_INTCON val: 1
+#-----------------------------------------------------------------------------
+# INTERMEDIATE CODE FOR FUNCTION: main
+#-----------------------------------------------------------------------------
+# temps used:    3
+# svregs used:   0
+# stktemps used: 0
+# regargs used:  1
+# stkargs used:  0
+# CNT_LI
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_INTCON (0)
+# |
+# >
+# CNT_SW
+# ------
+# dest -> ST_PRIMITIVE (i)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_LABEL
+# ---------
+# src1 -> ST_LABEL (0)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (i)
+# |
+# >
+# CNT_LI
+# ------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_INTCON (100)
+# |
+# >
+# CNT_SLT
+# -------
+# dest -> ST_TEMPORARY (2)
+# src1 -> ST_TEMPORARY (0)
+# src2 -> ST_TEMPORARY (1)
+# |
+# >
+# CNT_CNDJMP
+# ----------
+# dest -> ST_LABEL (1)
+# src1 -> ST_TEMPORARY (2)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (i)
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_int)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_STRCON ("\n")
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_string)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (i)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_PRIMITIVE (i)
+# |
+# >
+# CNT_ADD
+# -------
+# dest -> ST_TEMPORARY (2)
+# src1 -> ST_TEMPORARY (0)
+# src2 -> ST_TEMPORARY (1)
+# |
+# >
+# CNT_LI
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_INTCON (1)
+# |
+# >
+# CNT_ADD
+# -------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_TEMPORARY (2)
+# src2 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_SW
+# ------
+# dest -> ST_PRIMITIVE (i)
+# src1 -> ST_TEMPORARY (1)
+# |
+# >
+# CNT_JUMP
+# --------
+# dest -> ST_LABEL (0)
+# |
+# >
+# CNT_LABEL
+# ---------
+# src1 -> ST_LABEL (1)
+
+.data
+
+  _i:
+    .space 4
+
+  _str0:
+    .asciiz "\n"
+    .align 2
+
+.text
+
+main:
+
+  # |--------------------------------|
+  # |           <padding>            |
+  # |--------------------------------| sp+20
+  # |              $ra               |
+  # |--------------------------------| sp+16
+  # |     <minimum 4 arg slots>      |
+  # |--------------------------------| sp+0
+  la $sp, -24($sp)
+  sw $ra, 16($sp)
+
+  li $t0, 0
+  sw $t0, _i
+_L0:
+  lw $t0, _i
+  li $t1, 100
+  slt $t2, $t0, $t1
+  beq $t2, 0, _L1
+  lw $t0, _i
+  move $a0, $t0
+  jal __print_int
+  la $t0, _str0
+  move $a0, $t0
+  jal __print_string
+  lw $t0, _i
+  lw $t1, _i
+  add $t2, $t0, $t1
+  li $t0, 1
+  add $t1, $t2, $t0
+  sw $t1, _i
+  j _L0
+_L1:
+
+__leavemain:
+  lw $ra, 16($sp)
+  la $sp, 24($sp)
+  jr $ra
+
+__print_string:
+  li $v0, 4
+  syscall
+  jr $ra
+
+__print_int:
+  li $v0, 1
+  syscall
+  jr $ra
+
+__print_char:
+  li $v0, 11
+  syscall
+  jr $ra
