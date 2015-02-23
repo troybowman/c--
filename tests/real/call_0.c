dbbefc904bf9ec27cb6060cb7b469f9ce16068f8
troybowman <troybowman2000@gmail.com>
2015-02-24 00:46:26 +0100

    + test for simple function calls

--- tests/real/call_0.c
+++ tests/real/call_0.c
@@ -5,8 +5,7 @@ int id(int x)
   return x;
 }
 
-extern void print_int (int n);
-extern void print_string (char s[]);
+extern void printf(char format[], ...);
 
 void main(void)
 {
@@ -15,6 +14,6 @@ void main(void)
   f = 1;
   for (n = 7; n > 0; n = n-1)
     f = id(n)*f;
-  print_int(f);
-  print_string("\n");
+
+  printf("%d\n", f);
 }
