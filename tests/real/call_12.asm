2c03356ff923eb7c600a078a7221c34be4908872
troybowman <troybowman2000@gmail.com>
2015-02-25 00:11:18 +0100

    + correct asm file for real/call_12.c

---
+++ tests/real/call_12.asm
@@ -0,0 +1,970 @@
+#-----------------------------------------------------------------------------
+# GLOBAL SYMBOL TABLE
+#-----------------------------------------------------------------------------
+# size: 8
+# sym: x
+#   line: 3
+#   type: ST_ARRAY
+#     base: PRIM_CHAR
+#     size: 0xf
+# sym: copy
+#   line: 5
+#   type: ST_FUNCTION
+#     rt_type: RT_INT
+#     params:
+#       0: a
+#         type: ST_ARRAY
+#           base: PRIM_CHAR
+#       1: b
+#         type: ST_ARRAY
+#           base: PRIM_CHAR
+#     is_extern: no
+# sym: sum
+#   line: 16
+#   type: ST_FUNCTION
+#     rt_type: RT_INT
+#     params:
+#       0: b
+#         type: ST_ARRAY
+#           base: PRIM_CHAR
+#     is_extern: no
+# sym: _print_int
+#   line: -1
+#   type: ST_FUNCTION
+#     rt_type: RT_VOID
+#     params:
+#       0: val
+#         type: ST_PRIMITIVE
+#           base: PRIM_INT
+#     is_extern: yes
+# sym: _print_string
+#   line: -1
+#   type: ST_FUNCTION
+#     rt_type: RT_VOID
+#     params:
+#       0: str
+#         type: ST_ARRAY
+#           base: PRIM_CHAR
+#     is_extern: yes
+# sym: _print_char
+#   line: -1
+#   type: ST_FUNCTION
+#     rt_type: RT_VOID
+#     params:
+#       0: c
+#         type: ST_PRIMITIVE
+#           base: PRIM_CHAR
+#     is_extern: yes
+# sym: printf
+#   line: 29
+#   type: ST_FUNCTION
+#     rt_type: RT_VOID
+#     params:
+#       0: format
+#         type: ST_ARRAY
+#           base: PRIM_CHAR
+#       1: ST_ELLIPSIS
+#     is_extern: yes
+# sym: main
+#   line: 31
+#   type: ST_FUNCTION
+#     rt_type: RT_VOID
+#     params:
+#       none
+#     is_extern: no
+#-----------------------------------------------------------------------------
+# LOCAL SYMBOLS FOR FUNCTION: copy
+#-----------------------------------------------------------------------------
+# size: 3
+# sym: a
+#   line: 5
+#   type: ST_ARRAY
+#     base: PRIM_CHAR
+#     size: 0xffffffff
+# sym: b
+#   line: 5
+#   type: ST_ARRAY
+#     base: PRIM_CHAR
+#     size: 0xffffffff
+# sym: i
+#   line: 7
+#   type: ST_PRIMITIVE
+#     base: PRIM_INT
+#-----------------------------------------------------------------------------
+# SYNTAX TREE FOR FUNCTION: copy
+#-----------------------------------------------------------------------------
+# node 1: type: TNT_STMT
+# child SEQ_CUR for node 1
+# node 2: type: TNT_ASSG
+# child LHS for node 2
+# node 3: type: TNT_SYMBOL sym: i
+# child RHS for node 2
+# node 4: type: TNT_INTCON val: 0
+# child SEQ_NEXT for node 1
+# node 5: type: TNT_STMT
+# child SEQ_CUR for node 5
+# node 6: type: TNT_FOR
+# child FOR_INIT for node 6
+# node 7: type: TNT_ASSG
+# child LHS for node 7
+# node 8: type: TNT_SYMBOL sym: i
+# child RHS for node 7
+# node 9: type: TNT_INTCON val: 0
+# child FOR_COND for node 6
+# node 10: type: TNT_GT
+# child LHS for node 10
+# node 11: type: TNT_ARRAY_LOOKUP sym: a
+# child AL_OFFSET for node 11
+# node 12: type: TNT_SYMBOL sym: i
+# child RHS for node 10
+# node 13: type: TNT_INTCON val: 0
+# child FOR_INC for node 6
+# node 14: type: TNT_ASSG
+# child LHS for node 14
+# node 15: type: TNT_SYMBOL sym: i
+# child RHS for node 14
+# node 16: type: TNT_PLUS
+# child LHS for node 16
+# node 17: type: TNT_SYMBOL sym: i
+# child RHS for node 16
+# node 18: type: TNT_INTCON val: 1
+# child FOR_BODY for node 6
+# node 19: type: TNT_ASSG
+# child LHS for node 19
+# node 20: type: TNT_ARRAY_LOOKUP sym: b
+# child AL_OFFSET for node 20
+# node 21: type: TNT_SYMBOL sym: i
+# child RHS for node 19
+# node 22: type: TNT_ARRAY_LOOKUP sym: a
+# child AL_OFFSET for node 22
+# node 23: type: TNT_SYMBOL sym: i
+# child SEQ_NEXT for node 5
+# node 24: type: TNT_STMT
+# child SEQ_CUR for node 24
+# node 25: type: TNT_ASSG
+# child LHS for node 25
+# node 26: type: TNT_ARRAY_LOOKUP sym: b
+# child AL_OFFSET for node 26
+# node 27: type: TNT_SYMBOL sym: i
+# child RHS for node 25
+# node 28: type: TNT_INTCON val: 0
+# child SEQ_NEXT for node 24
+# node 29: type: TNT_STMT
+# child SEQ_CUR for node 29
+# node 30: type: TNT_RET
+# child RET_EXPR for node 30
+# node 31: type: TNT_SYMBOL sym: i
+#-----------------------------------------------------------------------------
+# LOCAL SYMBOLS FOR FUNCTION: sum
+#-----------------------------------------------------------------------------
+# size: 3
+# sym: b
+#   line: 16
+#   type: ST_ARRAY
+#     base: PRIM_CHAR
+#     size: 0xffffffff
+# sym: i
+#   line: 18
+#   type: ST_PRIMITIVE
+#     base: PRIM_INT
+# sym: sum
+#   line: 18
+#   type: ST_PRIMITIVE
+#     base: PRIM_INT
+#-----------------------------------------------------------------------------
+# SYNTAX TREE FOR FUNCTION: sum
+#-----------------------------------------------------------------------------
+# node 1: type: TNT_STMT
+# child SEQ_CUR for node 1
+# node 2: type: TNT_ASSG
+# child LHS for node 2
+# node 3: type: TNT_SYMBOL sym: i
+# child RHS for node 2
+# node 4: type: TNT_INTCON val: 0
+# child SEQ_NEXT for node 1
+# node 5: type: TNT_STMT
+# child SEQ_CUR for node 5
+# node 6: type: TNT_ASSG
+# child LHS for node 6
+# node 7: type: TNT_SYMBOL sym: sum
+# child RHS for node 6
+# node 8: type: TNT_INTCON val: 0
+# child SEQ_NEXT for node 5
+# node 9: type: TNT_STMT
+# child SEQ_CUR for node 9
+# node 10: type: TNT_WHILE
+# child WHILE_COND for node 10
+# node 11: type: TNT_GT
+# child LHS for node 11
+# node 12: type: TNT_ARRAY_LOOKUP sym: b
+# child AL_OFFSET for node 12
+# node 13: type: TNT_SYMBOL sym: i
+# child RHS for node 11
+# node 14: type: TNT_INTCON val: 0
+# child WHILE_BODY for node 10
+# node 15: type: TNT_STMT
+# child SEQ_CUR for node 15
+# node 16: type: TNT_ASSG
+# child LHS for node 16
+# node 17: type: TNT_SYMBOL sym: sum
+# child RHS for node 16
+# node 18: type: TNT_PLUS
+# child LHS for node 18
+# node 19: type: TNT_SYMBOL sym: sum
+# child RHS for node 18
+# node 20: type: TNT_ARRAY_LOOKUP sym: b
+# child AL_OFFSET for node 20
+# node 21: type: TNT_SYMBOL sym: i
+# child SEQ_NEXT for node 15
+# node 22: type: TNT_STMT
+# child SEQ_CUR for node 22
+# node 23: type: TNT_ASSG
+# child LHS for node 23
+# node 24: type: TNT_SYMBOL sym: i
+# child RHS for node 23
+# node 25: type: TNT_PLUS
+# child LHS for node 25
+# node 26: type: TNT_SYMBOL sym: i
+# child RHS for node 25
+# node 27: type: TNT_INTCON val: 1
+# child SEQ_NEXT for node 9
+# node 28: type: TNT_STMT
+# child SEQ_CUR for node 28
+# node 29: type: TNT_RET
+# child RET_EXPR for node 29
+# node 30: type: TNT_SYMBOL sym: sum
+#-----------------------------------------------------------------------------
+# LOCAL SYMBOLS FOR FUNCTION: main
+#-----------------------------------------------------------------------------
+# size: 1
+# sym: y
+#   line: 33
+#   type: ST_PRIMITIVE
+#     base: PRIM_INT
+#-----------------------------------------------------------------------------
+# SYNTAX TREE FOR FUNCTION: main
+#-----------------------------------------------------------------------------
+# node 1: type: TNT_STMT
+# child SEQ_CUR for node 1
+# node 2: type: TNT_ASSG
+# child LHS for node 2
+# node 3: type: TNT_SYMBOL sym: y
+# child RHS for node 2
+# node 4: type: TNT_CALL sym: copy
+# child CALL_ARGS for node 4
+# node 5: type: TNT_ARG
+# child SEQ_CUR for node 5
+# node 6: type: TNT_STRCON str: "hello world"
+# child SEQ_NEXT for node 5
+# node 7: type: TNT_ARG
+# child SEQ_CUR for node 7
+# node 8: type: TNT_SYMBOL sym: x
+# child SEQ_NEXT for node 1
+# node 9: type: TNT_STMT
+# child SEQ_CUR for node 9
+# node 10: type: TNT_ASSG
+# child LHS for node 10
+# node 11: type: TNT_SYMBOL sym: y
+# child RHS for node 10
+# node 12: type: TNT_PLUS
+# child LHS for node 12
+# node 13: type: TNT_SYMBOL sym: y
+# child RHS for node 12
+# node 14: type: TNT_CALL sym: sum
+# child CALL_ARGS for node 14
+# node 15: type: TNT_ARG
+# child SEQ_CUR for node 15
+# node 16: type: TNT_SYMBOL sym: x
+# child SEQ_NEXT for node 9
+# node 17: type: TNT_STMT
+# child SEQ_CUR for node 17
+# node 18: type: TNT_PRINTF
+# child PRINTF_TREE for node 18
+# node 19: type: TNT_STMT
+# child SEQ_CUR for node 19
+# node 20: type: TNT_CALL sym: _print_int
+# child CALL_ARGS for node 20
+# node 21: type: TNT_ARG
+# child SEQ_CUR for node 21
+# node 22: type: TNT_SYMBOL sym: y
+# child SEQ_NEXT for node 19
+# node 23: type: TNT_STMT
+# child SEQ_CUR for node 23
+# node 24: type: TNT_CALL sym: _print_string
+# child CALL_ARGS for node 24
+# node 25: type: TNT_ARG
+# child SEQ_CUR for node 25
+# node 26: type: TNT_STRCON str: "\n"
+#-----------------------------------------------------------------------------
+# INTERMEDIATE CODE FOR FUNCTION: copy
+#-----------------------------------------------------------------------------
+# temps used:    4
+# svregs used:   0
+# stktemps used: 0
+# regargs used:  0
+# stkargs used:  0
+# CNT_LI
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_INTCON (0)
+# |
+# >
+# CNT_SW
+# ------
+# dest -> ST_PRIMITIVE (i)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_LI
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_INTCON (0)
+# |
+# >
+# CNT_SW
+# ------
+# dest -> ST_PRIMITIVE (i)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_LABEL
+# ---------
+# src1 -> ST_LABEL (0)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (i)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_ARRAY (a)
+# |
+# >
+# CNT_ADD
+# -------
+# dest -> ST_TEMPORARY (2)
+# src1 -> ST_TEMPORARY (1)
+# src2 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_LB
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_TEMPORARY (2)
+# |
+# >
+# CNT_LI
+# ------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_INTCON (0)
+# |
+# >
+# CNT_SGT
+# -------
+# dest -> ST_TEMPORARY (2)
+# src1 -> ST_TEMPORARY (0)
+# src2 -> ST_TEMPORARY (1)
+# |
+# >
+# CNT_CNDJMP
+# ----------
+# dest -> ST_LABEL (1)
+# src1 -> ST_TEMPORARY (2)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (i)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_ARRAY (a)
+# |
+# >
+# CNT_ADD
+# -------
+# dest -> ST_TEMPORARY (2)
+# src1 -> ST_TEMPORARY (1)
+# src2 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_LB
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_TEMPORARY (2)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_PRIMITIVE (i)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (2)
+# src1 -> ST_ARRAY (b)
+# |
+# >
+# CNT_ADD
+# -------
+# dest -> ST_TEMPORARY (3)
+# src1 -> ST_TEMPORARY (2)
+# src2 -> ST_TEMPORARY (1)
+# |
+# >
+# CNT_SB
+# ------
+# dest -> ST_TEMPORARY (3)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (i)
+# |
+# >
+# CNT_LI
+# ------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_INTCON (1)
+# |
+# >
+# CNT_ADD
+# -------
+# dest -> ST_TEMPORARY (2)
+# src1 -> ST_TEMPORARY (0)
+# src2 -> ST_TEMPORARY (1)
+# |
+# >
+# CNT_SW
+# ------
+# dest -> ST_PRIMITIVE (i)
+# src1 -> ST_TEMPORARY (2)
+# |
+# >
+# CNT_JUMP
+# --------
+# dest -> ST_LABEL (0)
+# |
+# >
+# CNT_LABEL
+# ---------
+# src1 -> ST_LABEL (1)
+# |
+# >
+# CNT_LI
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_INTCON (0)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_PRIMITIVE (i)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (2)
+# src1 -> ST_ARRAY (b)
+# |
+# >
+# CNT_ADD
+# -------
+# dest -> ST_TEMPORARY (3)
+# src1 -> ST_TEMPORARY (2)
+# src2 -> ST_TEMPORARY (1)
+# |
+# >
+# CNT_SB
+# ------
+# dest -> ST_TEMPORARY (3)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (i)
+# |
+# >
+# CNT_RET
+# -------
+# dest -> ST_RETVAL
+# src1 -> ST_TEMPORARY (0)
+#-----------------------------------------------------------------------------
+# INTERMEDIATE CODE FOR FUNCTION: sum
+#-----------------------------------------------------------------------------
+# temps used:    4
+# svregs used:   0
+# stktemps used: 0
+# regargs used:  0
+# stkargs used:  0
+# CNT_LI
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_INTCON (0)
+# |
+# >
+# CNT_SW
+# ------
+# dest -> ST_PRIMITIVE (i)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_LI
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_INTCON (0)
+# |
+# >
+# CNT_SW
+# ------
+# dest -> ST_PRIMITIVE (sum)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_LABEL
+# ---------
+# src1 -> ST_LABEL (0)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (i)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_ARRAY (b)
+# |
+# >
+# CNT_ADD
+# -------
+# dest -> ST_TEMPORARY (2)
+# src1 -> ST_TEMPORARY (1)
+# src2 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_LB
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_TEMPORARY (2)
+# |
+# >
+# CNT_LI
+# ------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_INTCON (0)
+# |
+# >
+# CNT_SGT
+# -------
+# dest -> ST_TEMPORARY (2)
+# src1 -> ST_TEMPORARY (0)
+# src2 -> ST_TEMPORARY (1)
+# |
+# >
+# CNT_CNDJMP
+# ----------
+# dest -> ST_LABEL (1)
+# src1 -> ST_TEMPORARY (2)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (sum)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_PRIMITIVE (i)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (2)
+# src1 -> ST_ARRAY (b)
+# |
+# >
+# CNT_ADD
+# -------
+# dest -> ST_TEMPORARY (3)
+# src1 -> ST_TEMPORARY (2)
+# src2 -> ST_TEMPORARY (1)
+# |
+# >
+# CNT_LB
+# ------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_TEMPORARY (3)
+# |
+# >
+# CNT_ADD
+# -------
+# dest -> ST_TEMPORARY (2)
+# src1 -> ST_TEMPORARY (0)
+# src2 -> ST_TEMPORARY (1)
+# |
+# >
+# CNT_SW
+# ------
+# dest -> ST_PRIMITIVE (sum)
+# src1 -> ST_TEMPORARY (2)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (i)
+# |
+# >
+# CNT_LI
+# ------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_INTCON (1)
+# |
+# >
+# CNT_ADD
+# -------
+# dest -> ST_TEMPORARY (2)
+# src1 -> ST_TEMPORARY (0)
+# src2 -> ST_TEMPORARY (1)
+# |
+# >
+# CNT_SW
+# ------
+# dest -> ST_PRIMITIVE (i)
+# src1 -> ST_TEMPORARY (2)
+# |
+# >
+# CNT_JUMP
+# --------
+# dest -> ST_LABEL (0)
+# |
+# >
+# CNT_LABEL
+# ---------
+# src1 -> ST_LABEL (1)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (sum)
+# |
+# >
+# CNT_RET
+# -------
+# dest -> ST_RETVAL
+# src1 -> ST_TEMPORARY (0)
+#-----------------------------------------------------------------------------
+# INTERMEDIATE CODE FOR FUNCTION: main
+#-----------------------------------------------------------------------------
+# temps used:    2
+# svregs used:   1
+# stktemps used: 0
+# regargs used:  2
+# stkargs used:  0
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_STRCON ("hello world")
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_ARRAY (x)
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (1)
+# src1 -> ST_TEMPORARY (1)
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# dest -> ST_RETVAL
+# src1 -> ST_FUNCTION (copy)
+# |
+# >
+# CNT_MOV
+# -------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_RETVAL
+# |
+# >
+# CNT_SW
+# ------
+# dest -> ST_PRIMITIVE (y)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_SAVED_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (y)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_ARRAY (x)
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# dest -> ST_RETVAL
+# src1 -> ST_FUNCTION (sum)
+# |
+# >
+# CNT_MOV
+# -------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_RETVAL
+# |
+# >
+# CNT_ADD
+# -------
+# dest -> ST_TEMPORARY (1)
+# src1 -> ST_SAVED_TEMPORARY (0)
+# src2 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_SW
+# ------
+# dest -> ST_PRIMITIVE (y)
+# src1 -> ST_TEMPORARY (1)
+# |
+# >
+# CNT_LW
+# ------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_PRIMITIVE (y)
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_int)
+# |
+# >
+# CNT_LEA
+# -------
+# dest -> ST_TEMPORARY (0)
+# src1 -> ST_STRCON ("\n")
+# |
+# >
+# CNT_ARG
+# -------
+# dest -> ST_REG_ARGUMENT (0)
+# src1 -> ST_TEMPORARY (0)
+# |
+# >
+# CNT_CALL
+# --------
+# src1 -> ST_FUNCTION (_print_string)
+
+.data
+
+  _x:
+    .space 15
+    .align 2
+
+  _str0:
+    .asciiz "hello world"
+    .align 2
+
+  _str1:
+    .asciiz "\n"
+    .align 2
+
+.text
+
+_copy:
+
+  # |--------------------------------|
+  # |         <b is in $a1>          |
+  # |--------------------------------| sp+12
+  # |         <a is in $a0>          |
+  # |--------------------------------| sp+8  <-- start of caller's stack
+  # |           <padding>            |
+  # |--------------------------------| sp+4
+  # |               i                |
+  # |--------------------------------| sp+0
+  la $sp, -8($sp)
+
+  li $t0, 0
+  sw $t0, 0($sp)
+  li $t0, 0
+  sw $t0, 0($sp)
+_L0:
+  lw $t0, 0($sp)
+  move $t1, $a0
+  add $t2, $t1, $t0
+  lb $t0, ($t2)
+  li $t1, 0
+  sgt $t2, $t0, $t1
+  beq $t2, 0, _L1
+  lw $t0, 0($sp)
+  move $t1, $a0
+  add $t2, $t1, $t0
+  lb $t0, ($t2)
+  lw $t1, 0($sp)
+  move $t2, $a1
+  add $t3, $t2, $t1
+  sb $t0, ($t3)
+  lw $t0, 0($sp)
+  li $t1, 1
+  add $t2, $t0, $t1
+  sw $t2, 0($sp)
+  j _L0
+_L1:
+  li $t0, 0
+  lw $t1, 0($sp)
+  move $t2, $a1
+  add $t3, $t2, $t1
+  sb $t0, ($t3)
+  lw $t0, 0($sp)
+  move $v0, $t0
+  j __leave_copy
+
+__leave_copy:
+  la $sp, 8($sp)
+  jr $ra
+
+_sum:
+
+  # |--------------------------------|
+  # |         <b is in $a0>          |
+  # |--------------------------------| sp+8  <-- start of caller's stack
+  # |              sum               |
+  # |--------------------------------| sp+4
+  # |               i                |
+  # |--------------------------------| sp+0
+  la $sp, -8($sp)
+
+  li $t0, 0
+  sw $t0, 0($sp)
+  li $t0, 0
+  sw $t0, 4($sp)
+_L2:
+  lw $t0, 0($sp)
+  move $t1, $a0
+  add $t2, $t1, $t0
+  lb $t0, ($t2)
+  li $t1, 0
+  sgt $t2, $t0, $t1
+  beq $t2, 0, _L3
+  lw $t0, 4($sp)
+  lw $t1, 0($sp)
+  move $t2, $a0
+  add $t3, $t2, $t1
+  lb $t1, ($t3)
+  add $t2, $t0, $t1
+  sw $t2, 4($sp)
+  lw $t0, 0($sp)
+  li $t1, 1
+  add $t2, $t0, $t1
+  sw $t2, 0($sp)
+  j _L2
+_L3:
+  lw $t0, 4($sp)
+  move $v0, $t0
+  j __leave_sum
+
+__leave_sum:
+  la $sp, 8($sp)
+  jr $ra
+
+main:
+
+  # |--------------------------------|
+  # |           <padding>            |
+  # |--------------------------------| sp+28
+  # |               y                |
+  # |--------------------------------| sp+24
+  # |              $ra               |
+  # |--------------------------------| sp+20
+  # |              $s0               |
+  # |--------------------------------| sp+16
+  # |     <minimum 4 arg slots>      |
+  # |--------------------------------| sp+0
+  la $sp, -32($sp)
+  sw $ra, 20($sp)
+  sw $s0, 16($sp)
+
+  la $t0, _str0
+  la $t1, _x
+  move $a1, $t1
+  move $a0, $t0
+  jal _copy
+  move $t0, $v0
+  sw $t0, 24($sp)
+  lw $s0, 24($sp)
+  la $t0, _x
+  move $a0, $t0
+  jal _sum
+  move $t0, $v0
+  add $t1, $s0, $t0
+  sw $t1, 24($sp)
+  lw $t0, 24($sp)
+  move $a0, $t0
+  jal __print_int
+  la $t0, _str1
+  move $a0, $t0
+  jal __print_string
+
+__leavemain:
+  lw $s0, 16($sp)
+  lw $ra, 20($sp)
+  la $sp, 32($sp)
+  jr $ra
+
+__print_string:
+  li $v0, 4
+  syscall
+  jr $ra
+
+__print_int:
+  li $v0, 1
+  syscall
+  jr $ra
+
+__print_char:
+  li $v0, 11
+  syscall
+  jr $ra
