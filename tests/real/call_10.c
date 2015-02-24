79c77c10aa1dd70a60e3fff75da983e4e0812db4
troybowman <troybowman2000@gmail.com>
2015-02-24 22:32:38 +0100

    + test for int=>char function parameters

--- tests/real/call_10.c
+++ tests/real/call_10.c
@@ -7,14 +7,13 @@ char f(char x)
   return x+1;
 }
 
-extern void print_int (int n);
-extern void print_string (char s[]);
+extern void printf(char format[], ...);
 
 void main(void)
 {
   x = 12345;
   x = f(x);
   x = x+f(x);
-  print_int(x);
-  print_string("\n");
+
+  printf("%d\n", x);
 }
