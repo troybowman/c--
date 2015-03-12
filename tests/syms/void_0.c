f123235b398a0448f926cc07fa507f229f5c8ee5
troybowman <troybowman2000@gmail.com>
2015-03-12 23:27:13 +0100

    + test for void detecting void variables, which are now syntactically possible

---
+++ tests/syms/void_0.c
@@ -0,0 +1,24 @@
+int x, y;
+
+void wrong;
+
+char a;
+
+int wrong, wrong;
+
+void func(int x, char y, void badarg, char hello[]);
+
+void evenworse[50];
+
+void func2(void x, void y[], void z)
+{
+
+}
+
+int main(void)
+{
+  void wrong;
+  int wrong, wrong;
+  char x;
+  void ugh[2];
+}
