692c0d75ac9829dd812fffdfc74c7e6acc40658d
troybowman <troybowman2000@gmail.com>
2015-03-15 16:45:54 +0100

    added simplest-case sanity checks

---
+++ tests/syms/sanity_1.c
@@ -0,0 +1,1 @@
+char string[10];
---
+++ tests/syms/sanity_3.asm
@@ -0,0 +1,13 @@
+#-----------------------------------------------------------------------------
+# GLOBAL SYMBOL TABLE
+#-----------------------------------------------------------------------------
+# size: 1
+# sym: main
+#   line: 1
+#   type: ST_FUNCTION
+#     rt_type: PRIM_VOID
+#     params:
+#       0: x
+#         type: ST_PRIMITIVE
+#           base: PRIM_INT
+#     is_extern: no
