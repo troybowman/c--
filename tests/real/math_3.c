e6d955ed19dbc8d51acb8d4c6719a023039f996a
troybowman <troybowman2000@gmail.com>
2015-07-13 22:19:59 +0200

    + test for binary operators

---
+++ tests/real/math_3.c
@@ -0,0 +1,40 @@
+extern void printf(char format[], ...);
+
+void main(void)
+{
+  printf("%d ",  1 << 0);
+  printf("%d ",  1 << 1);
+  printf("%d ",  1 << 2);
+  printf("%d ",  1 << 3);
+  printf("%d\n", 1 << 4);
+
+  printf("%d ",  8 >> 0);
+  printf("%d ",  8 >> 1);
+  printf("%d ",  8 >> 2);
+  printf("%d ",  8 >> 3);
+  printf("%d\n", 8 >> 4);
+
+  printf("%d ",  1 & 0);
+  printf("%d ",  1 & 1);
+  printf("%d ",  1 & 2);
+  printf("%d ",  1 & 3);
+  printf("%d\n", 1 & 4);
+
+  printf("%d ",  1 | 0);
+  printf("%d ",  1 | 1);
+  printf("%d ",  1 | 2);
+  printf("%d ",  1 | 3);
+  printf("%d\n", 1 | 4);
+
+  printf("%d ",  0 ^ 0);
+  printf("%d ",  1 ^ 0);
+  printf("%d ",  2 ^ 1);
+  printf("%d ",  3 ^ 2);
+  printf("%d\n", 4 ^ 3);
+
+  printf("%d ",  ~0);
+  printf("%d ",  ~1);
+  printf("%d ",  ~2);
+  printf("%d ",  ~3);
+  printf("%d\n", ~4);
+}
