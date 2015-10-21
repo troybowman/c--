cbbd51a0db1fbdaaef09087a1d7125d360994f1f
troybowman <troybowman2000@gmail.com>
2015-10-21 21:51:45 +0200

    saving work

--- include/asm.h
+++ include/asm.h
@@ -1,5 +1,5 @@
-#ifndef MIPS_ASM_H
-#define MIPS_ASM_H
+#ifndef ASM_H
+#define ASM_H
 
 #include <ir.h>
 
@@ -10,14 +10,66 @@ class frame_section_t;
 void generate_mips_asm(asm_contex_t &ctx, ir_t &ir);
 
 //-----------------------------------------------------------------------------
+typedef uint32_t dbg_flags_t;
+
+//-----------------------------------------------------------------------------
+enum dbg_flag_t
+{
+  dbg_no_parse      = 1 << 0, // just collect cmdline args and exit
+  dbg_print_gsyms   = 1 << 1, // print global symbol table
+  dbg_print_structs = 1 << 2, // print structures table
+  dbg_print_funcs   = 1 << 3, // print syntax tree and lvars for each function
+  dbg_no_ir         = 1 << 4, // exit before generating intermediate code
+  dbg_print_ir      = 1 << 5, // print intermediate code
+  dbg_no_code       = 1 << 6, // exit before final code generation
+  dbg_asm_cmts      = 1 << 7, // include debug messages in asm output
+};
+
+//-----------------------------------------------------------------------------
+class asm_context_t
+{
+  FILE *outfile;
+  dbg_flags_t flags;
+
+  // all named symbols (labels, strings, global vars, and functions)
+  symtab_t syms;
+
+  // reserved temps. used for storing temporaries in memory when we
+  // run out of temp registers
+  symref_t t7;
+  symref_t t8;
+  symref_t t9;
+
+  int tablevel;
+
+  void print_gsyms(const symtab_t &);
+  void print_trees(const stx_trees_t &);
+  void print_structs(const structab_t &);
+  void print_stack_frame(const stack_frame_t &frame);
+
+  void out(int tablevel, const char *fmt, ...);
+  void out(const char *fmt, ...);
+  void outcmt(const char *fmt, ...);
+  void outcmt(int tablevel, const char *fmt, ...);
+
+public:
+  asm_context_t(FILE *_outfile, dbg_flags_t _flags);
+
+  void print_parse_results(const parse_results_t &res);
+  void print_ir(const ir_t &ir);
+
+  void init_named_symbols(const ir_t &ir);
+
+  void gen_data_section();
+  void gen_text_section(ir_funcs_t &ir);
+};
+
+//-----------------------------------------------------------------------------
 struct item_info_t
 {
   asm_context_t &actx;
   const area_t &sec;
-  const uint32_t idx;
-
-  item_info_t(asm_context_t &_actx, const area_t &_sec, uint32_t _idx)
-    : actx(_actx), area(_sec), idx(_idx) {}
+  uint32_t idx;
 };
 
 //-----------------------------------------------------------------------------
@@ -30,15 +82,12 @@ struct frame_item_visitor_t
 struct frame_mods_t : public item_info_t
 {
   offset_t delta;
-
-  frame_mods_t(asm_context_t &actx, const area_t &sec, uint32_t idx)
-    : delta(BADOFFSET), item_info_t(actx, sec, idx) {}
 };
 
 //-----------------------------------------------------------------------------
 struct frame_item_builder_t
 {
-  virtual void visit_item(frame_mods_t &mods, symbol_t &item) = 0;
+  virtual void build_item(frame_mods_t &mods, symbol_t &item) = 0;
 };
 
 //-----------------------------------------------------------------------------
@@ -56,7 +105,7 @@ public:
 
 #define FIV_REVERSE 0x1
   void visit_items(asm_context_t &, frame_item_visitor_t &, uint32_t flags = 0) const;
-  void visit_items(frame_item_builder_t &);
+  void build_items(asm_context_t &, frame_item_builder_t &);
 
   size_t nitems() const { return items.size(); }
   bool is_valid() const { return size() >= WORDSIZE; }
@@ -64,7 +113,7 @@ public:
 };
 
 //-----------------------------------------------------------------------------
-// frame section indices
+// frame section indexes
 #define FS_PARAMS   8
 #define FS_PADDING2 7
 #define FS_LVARS    6
@@ -80,7 +129,6 @@ struct stack_frame_t
 {
   frame_section_t sections[9];
   symref_t epilogue_label;
-
   stack_frame_t(asm_context_t &_ctx, const ir_func_t &func);
 
   offset_t size() const { return sections[FS_PADDING2].end; }
@@ -89,4 +137,20 @@ struct stack_frame_t
   void gen_epilogue();
 };
 
-#endif // MIPS_ASM_H
+//-----------------------------------------------------------------------------
+struct reg_saver_t : public frame_item_visitor_t
+{
+  const char *cmd;
+  frame_section_t &base;
+
+  virtual void visit_item(item_info_t &info, const symbol_t &sym)
+  {
+    info.actx.out(TAB1,
+        "%s %s, %d($sp)\n",
+        cmd,
+        sym.loc.reg(),
+        base.start + info.idx * WORDSIZE);
+  }
+};
+
+#endif // ASM_H
